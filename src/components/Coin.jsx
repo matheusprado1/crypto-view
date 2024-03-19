8187a0b0aa9f8d8f01fe817042eaf524d46897f5
matheusprado1 <matheusprado1096@gmail.com>
2024-03-19 16:24:49 -0300

    feat: update Coin component

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -8,7 +8,7 @@ const Coin = ({ coin }) => {
     <Link to={`/coin/${coin.id}`}>
       <div className="grid grid-cols-3 sm:grid-cols-4 font-light p-2 rounded border-gray-200 border-b hover:bg-gray-200">
         <div className="flex item-center gap-1 w-full">
-          <img className="w-6" src={coin.image} alt={coin.name} />
+          <img className="w-8 h-8" src={coin.image} alt={coin.name} />
           <p>{coin.name}</p>
           <span className="text-xs">({coin.symbol})</span>
         </div>
@@ -18,7 +18,7 @@ const Coin = ({ coin }) => {
           {coin.price_change_percentage_24h}
         </span>
         <div className="hidden sm:block">
-          <p className="font-semibold">Market Cap</p>
+          <p className="font-semibold">Valor de Mercado</p>
           <span>{formatBRLCurrency(coin.market_cap)}</span>
         </div>
       </div>
