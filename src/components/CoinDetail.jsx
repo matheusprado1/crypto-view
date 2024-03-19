5b8535b0ae92339268f0a7f4c00686f77aa35b39
matheusprado1 <matheusprado1096@gmail.com>
2024-03-19 16:26:07 -0300

    feat: update Coin Detail component

--- src/components/CoinDetail.jsx
+++ src/components/CoinDetail.jsx
@@ -8,7 +8,7 @@ const CoinDetail = () => {
   const { id } = useParams();
 
   const { response } = useAxios(`/coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&sparkline=false`);
-  console.log(response)
+  // console.log(response)
 
   if (!response) {
     return <div>Carregando...</div>
@@ -16,16 +16,32 @@ const CoinDetail = () => {
 
   return (
     <div className="m-6">
-      <div className="flex gap-2 items-center">
-        <img src={response.image.small} alt={response.name} />
-        <h1 className="text-2xl mb-2 capitalize font-bold">{response.name}</h1>
-        <span>Preço Atual: {formatBRLCurrency(response.market_data.current_price.brl)}</span>
-        <span className="text-green-400">Alta em 24Hrs: <TrendingUp />{formatBRLCurrency(response.market_data.high_24h.brl)}</span>
-        <span className="text-red-500">Baixa em 24Hrs: <TrendingDown />{formatBRLCurrency(response.market_data.low_24h.brl)}</span>
-        <span>Volume de Mercado {formatBRLCurrency(response.market_data.total_volume.brl)}</span>
+      <div className="flex flex-col gap-2 items-start">
+        <div className="flex gap-2 items-center">
+          <img src={response.image.small} alt={response.name} />
+          <div className="flex item-center gap-1 w-full">
+            <h1 className="text-2xl mb-2 capitalize font-bold">{response.name}
+            </h1>
+            <span className="text-xl">({response.symbol})</span>
+          </div>
+        </div>
+        <div>
+          <span className="font-semibold">Preço Atual: </span>
+          <span>
+            {formatBRLCurrency(response.market_data.current_price.brl)}
+          </span>
+        </div>
+        <div className="flex item-center gap-5 w-full">
+          <span className="text-green-400">Alta em 24Hrs: <TrendingUp />{formatBRLCurrency(response.market_data.high_24h.brl)}</span>
+          <span className="text-red-500">Baixa em 24Hrs: <TrendingDown />{formatBRLCurrency(response.market_data.low_24h.brl)}</span>
+        </div>
+        <div>
+          <span className="font-semibold">Volume de Mercado: </span>
+          <span>{formatBRLCurrency(response.market_data.total_volume.brl)}</span>
+        </div>
       </div>
 
-    </div>
+    </div >
   )
 }
 
