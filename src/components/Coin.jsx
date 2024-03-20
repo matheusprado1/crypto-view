d3ae688be13f0730092ccdac75d764b6239e9a35
matheusprado1 <matheusprado1096@gmail.com>
2024-03-20 10:49:30 -0300

    feat: update styles in Coin component

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -6,20 +6,25 @@ const Coin = ({ coin }) => {
   // console.log(coin)
   return (
     <Link to={`/coin/${coin.id}`}>
-      <div className="grid grid-cols-3 sm:grid-cols-4 font-light p-2 rounded border-gray-200 border-b hover:bg-gray-200">
-        <div className="flex item-center gap-1 w-full">
+      <div className="grid grid-cols-2 sm:grid-cols-4 font-light p-2 rounded border-gray-200 border-b hover:bg-gray-200">
+        <div className="flex item-center gap-2 w-full">
+          <span className="font-semibold">{coin.market_cap_rank}.</span>
           <img className="w-8 h-8" src={coin.image} alt={coin.name} />
           <p>{coin.name}</p>
           <span className="text-xs">({coin.symbol})</span>
         </div>
-        <span className="w-full text-center">{formatBRLCurrency(coin.current_price)}</span>
-        <span className={`flex gap-2 ${coin.price_change_percentage_24h < 0 ? "text-red-400" : "text-green-400"}`}>
-          {coin.price_change_percentage_24h < 0 ? <TrendingDown /> : <TrendingUp />}
-          {coin.price_change_percentage_24h}
-        </span>
         <div className="hidden sm:block">
-          <p className="font-semibold">Valor de Mercado</p>
-          <span>{formatBRLCurrency(coin.market_cap)}</span>
+          <span className="w-full text-center">{formatBRLCurrency(coin.current_price)}</span>
+        </div>
+        <div className="hidden sm:block">
+          <span className={`flex gap-2 ${coin.price_change_percentage_24h < 0 ? "text-red-400" : "text-green-400"}`}>
+            {coin.price_change_percentage_24h < 0 ? <TrendingDown /> : <TrendingUp />}
+            {coin.price_change_percentage_24h}
+          </span>
+        </div>
+        <div className="ml-7">
+          <p className="font-semibold">Preço Atual</p>
+          <span>{formatBRLCurrency(coin.current_price)}</span>
         </div>
       </div>
     </Link>
