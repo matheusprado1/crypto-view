5ae15e05acf69980417bd0298c912c577227bc35
matheusprado1 <matheusprado1096@gmail.com>
2024-03-21 12:22:27 -0300

    refactor: added browser router and provider involving app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
+import store from "./redux/store.js"
+import "./index.css"
+import axios from "axios"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+axios.defaults.baseURL = "https://crypto-server-3wsd.onrender.com";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
+
 )
